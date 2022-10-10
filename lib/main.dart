4e0e61bb1184a716d5716b65db7b0e05c1c587cd
martineelucky1996@gmail.com <martin@code.ke>
2022-10-10 11:41:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text('martin app'),
+      ),
+      body: Center(
+        child: Image.asset(
+            'images/105-1058683_download-happy-person-png-images-transparent-gallery-happy.png'),
+      ),
+    );
+  }
+}
